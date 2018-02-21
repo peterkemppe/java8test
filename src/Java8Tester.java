d80f04f8b50b9529e785b9c31db490bd1875715d
Peter Kemppe <peter@kemppe.se>
2018-02-21 07:35:43 +0100

    test

---
+++ src/Java8Tester.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.Map;
+
+public class Java8Tester {
+
+    public static void main(String args[]) {
+        Java8Tester tester = new Java8Tester();
+
+        //with type declaration
+        MathOperation addition = (int a, int b) -> a + b;
+
+        //with out type declaration
+        MathOperation subtraction = (a, b) -> a - b;
+
+        //with return statement along with curly braces
+        MathOperation multiplication = (int a, int b) -> { return a * b; };
+
+        //without return statement and without curly braces
+        MathOperation division = (int a, int b) -> a / b;
+
+        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+
+        //without parenthesis
+        GreetingService greetService1 = message ->
+                System.out.println("Hello " + message);
+
+        //with parenthesis
+        GreetingService greetService2 = (message) ->
+                System.out.println("Hello " + message);
+
+        greetService1.sayMessage("Mahesh");
+        greetService2.sayMessage("Suresh");
+
+        ShoutOutLoad shout1 = message -> System.out.println(message.toUpperCase());
+        ShoutOutLoad shout2 = message -> System.out.println(message.toLowerCase());
+
+        shout1.shoutMessage("liTE skrikigt");
+        shout2.shoutMessage("VISKA liTE");
+        System.out.println("MEN VAD händer?");
+    }
+
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+
+    interface ShoutOutLoad {
+        void shoutMessage(String message);
+    }
+
+    private int operate(int a, int b, MathOperation mathOperation) {
+        return mathOperation.operation(a, b);
+    }
+}
